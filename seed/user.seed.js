3e4386db309a7269a3ab52222a61da0f77177f57
Akaljot Gujjar <akaljotgujjar@gmail.com>
2020-03-23 23:43:40 -0700

    created an API and database

--- seed/user.seed.js
+++ seed/user.seed.js
@@ -1,8 +1,8 @@
 [{
   "id": 1,
-  "email": "lphan@shift3tech.com",
-  "username": "lphan",
-  "password": "abc123",
+  "email": "akaljotgujjar@mail.fresnostate.edu",
+  "username": "akaljotgujjar",
+  "password": "Babajiyouth22!",
   "super_admin": true,
   "site_admin": true,
   "hash": "CVI2C3WKYEdbL4yi3B9eE",
